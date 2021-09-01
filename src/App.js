2f615471542a2d09285f9bdab3f1bf8efbbcd4eb
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-09-01 22:05:24 +0200

    router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import ShowMovies from './components/ShowMovies';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+  <Router>
+      
+      <Route path="/" exact component={ShowMovies}/>
+      
 
-      <ShowMovies />
+  </Router>
+
       
     </div>
   );
