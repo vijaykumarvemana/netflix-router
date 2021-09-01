b93c143c57ff3c3d8a9ebf744480f184e88f893c
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-09-01 23:55:14 +0200

    Update ShowMovies.jsx

--- src/components/ShowMovies.jsx
+++ src/components/ShowMovies.jsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
+import { Link } from 'react-router-dom'
 const ShowMovies = () => {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(false)
   const getMovies = async () => {
     try {
       const response = await fetch(
@@ -31,11 +33,13 @@ const ShowMovies = () => {
     <Container md={2} className="mt-5">
       <Row >
         {movies.slice(0,6).map((movie) => (
-          <Col xs={6} md={2}>
-            <Card>
+          <Col xs={6} md={2} key={movie.id}>
+              <Link to='/'>
+            <Card onClick={() => setSelectedMovie(!selectedMovie)}>
               <Card.Img variant="top" src={movie.Poster}  />
               <Card.Title>{movie.Title}</Card.Title>
             </Card>
+              </Link>
           </Col>
         ))}
       </Row>
